7fa97493053019d9b3e4130675dba1c861123f72
MartianDreamer <nguyenxuansang9494@gmail.com>
2025-10-17 17:17:27 +0700

    testutils

--- testutils.py
+++ testutils.py
@@ -1,5 +1,14 @@
-def assert_result(description, expected, actual):
+def assert_equals(description, expected, actual):
 	if actual == expected:
 		quick_print(description + " - PASSED")
 	else:
 		quick_print(description + " - FALED")
+
+def assert_true(description, actual):
+	if actual:
+		quick_print(description + " - PASSED")
+	else:
+		quick_print(description + " - FALED")
+
+def assert_false(description, actual):
+	assert_true(description, not actual)
